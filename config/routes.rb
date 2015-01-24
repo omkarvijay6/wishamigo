82f105428f185a5fc2713a735283ac96635087d0
omkarvijay6 <omkarvijay6@gmail.com>
2015-01-24 17:20:38 +0530

    added occassions route and html

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root :to => "home#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
+  get 'occasions' => 'home#occasions'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
