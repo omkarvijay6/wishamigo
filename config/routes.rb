90d887a9751dfac4744225bbebe1e514ee200ea6
omkarvijay6 <omkarvijay6@gmail.com>
2015-01-24 20:49:11 +0530

    added about us root

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   
   get 'occasions' => 'home#occasions'
+  get 'AboutUs' => 'home#about_us', as: 'about_us'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
