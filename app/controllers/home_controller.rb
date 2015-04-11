9cb2ca40beba3b06577219dc13494e4cec6a0eba
omkarvijay6 <omkarvijay6@gmail.com>
2015-04-11 13:53:16 +0530

    added contact_us action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,5 +11,18 @@ class HomeController < ApplicationController
     end
 
     def contact_us
+    	@contact_us = ContactUs.new(contact_us_params)
+    	if @contact_us.save
+    		flash.notice = "Thank you for your request. Our team will contact you within 24 hours"
+    		redirect_to home_path
+    	else
+            @scroll_to = '#contact_us'
+            render 'home'
+    	end
+    end
+
+    private
+    def contact_us_params
+    	params.require(:contact_us).permit!
     end
 end
