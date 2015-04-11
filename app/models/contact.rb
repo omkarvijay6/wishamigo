2debd2dd31b9c3ad205f0362cf395cc2025fc60d
omkarvijay6 <omkarvijay6@gmail.com>
2015-04-11 23:23:47 +0530

    added email valid regex and uniqueness constrain

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,8 @@
 class Contact < ActiveRecord::Base
-  validates :name, :email, :phone, :occasion, presence: true
+  validates :name, :phone, :occasion, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
 end
