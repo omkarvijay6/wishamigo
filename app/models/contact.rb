74aaf70a4923d652ce54552a6c8a34b1a26675c0
omkarvijay6 <omkarvijay6@gmail.com>
2015-04-22 22:53:38 +0530

    added validation for phone number

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,10 @@
 class Contact < ActiveRecord::Base
-  validates :name, :phone, :occasion, :event_date, presence: true
+  validates :name, :occasion, :event_date, presence: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
+  validates :phone, presence: true,
+            :numericality => true,
+            :length => {:minimum => 10}
 end
