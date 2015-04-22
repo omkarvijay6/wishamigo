a3c43c0515a2324596bf8dcb123ece5ad2628804
omkarvijay6 <omkarvijay6@gmail.com>
2015-04-22 22:23:01 +0530

    made contact form submit to send mail

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,7 +14,9 @@ class HomeController < ApplicationController
     	@contact = Contact.new(contact_params)
     	if @contact.save
     		flash.keep[:success] = "Thank you for your request. Our team will contact you within 24 hours"
-    		redirect_to root_path
+            contact_mail = ContactMailer.welcome_email
+            contact_mail.deliver
+            redirect_to root_path
     	else
             @scroll_to = '#contact_us'
             render 'home'
