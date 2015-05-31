83f01c46c3fb06d62b95370cea9284e3cb753644
omkarvijay6 <omkarvijay6@gmail.com>
2015-05-31 15:27:05 +0530

    changed the design of the site: added slider, changed occassions, team, about

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require turbolinks
 //= require home
 //= require moment
 //= require bootstrap-datetimepicker
